393f9ed4e7754f19fd93f778010bdcd15b36d262
ANDREAS VLCEK <andreas.vlcek@googlemail.com>
2023-05-05 19:35:58 +0200

    added loading screen

--- Racing Game/Main.js
+++ Racing Game/Main.js
@@ -3,7 +3,10 @@ var canvas, canvasContext;
 window.onload = function() {
 	canvas = document.getElementById("gameCanvas");
 	canvasContext = canvas.getContext("2d");
-		
+	
+	colorRect(0, 0, canvas.width, canvas.height, "black");
+	colorText("Loading images", canvas.width / 2, canvas.height / 2);
+	
 	loadImages();
 }
 
